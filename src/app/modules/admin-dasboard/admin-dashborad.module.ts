c5307015b64502594caaf374d4bb29409c3a36c4
Mohamed Gamal <62777579+MahamedRadwan@users.noreply.github.com>
2021-04-30 21:57:41 +0200

    Admin-dashboard Done

--- src/app/modules/admin-dasboard/admin-dashborad.module.ts
+++ src/app/modules/admin-dasboard/admin-dashborad.module.ts
@@ -1,15 +1,17 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-
 import { AdminDashboradRoutingModule } from './admin-dashborad-routing.module';
-
+import {AccordionModule} from 'primeng/accordion';
+import { ChartModule } from 'primeng/chart';
 
 
 @NgModule({
   declarations: [...AdminDashboradRoutingModule.componentDeclartion],
   imports: [
     CommonModule,
-    AdminDashboradRoutingModule
+    AdminDashboradRoutingModule,
+    ChartModule, AccordionModule
+    
   ]
 })
 export class AdminDashboradModule { }
