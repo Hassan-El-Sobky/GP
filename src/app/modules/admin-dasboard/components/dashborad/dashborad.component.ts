44132fa88d4ea4cf904b4611452fcd37927c59ce
Mohamed Gamal <62777579+MahamedRadwan@users.noreply.github.com>
2021-05-08 19:18:01 +0200

    admin-dashboard Finished

--- src/app/modules/admin-dasboard/components/dashborad/dashborad.component.ts
+++ src/app/modules/admin-dasboard/components/dashborad/dashborad.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { AdminDashboradService } from '../../services/admin-dashborad.service'
 
 @Component({
   selector: 'app-dashborad',
@@ -8,25 +9,61 @@ import { Component, OnInit } from '@angular/core';
 export class DashboradComponent implements OnInit {
 
   data: any;
+  numOfSutudents: any;
+  numOfInstructors: any;
+  numOfCourse: any;
 
-  constructor() {
-      this.data = {
-          labels: ['A','B','C'],
-          datasets: [
-              {
-                  data: [300, 50, 100],
-                  backgroundColor: [
-                      "#FF6384",
-                      "#36A2EB",
-                      "#FFCE56"
-                  ],
-                  hoverBackgroundColor: [
-                      "#FF6384",
-                      "#36A2EB",
-                      "#FFCE56"
-                  ]
-              }]    
-          };
+  constructor(private _adminServices: AdminDashboradService) {
+
+
+
+    this.getAllInstructors();
+    this.getAllCourse();
+    this.getAllStudents();
+
+  }
+
+  getAllInstructors() {
+    this._adminServices.getAllInstructors().subscribe((data) => {
+      this.numOfInstructors = data.numOfInstructors;
+      this.charts();
+    });
+
+  }
+  getAllCourse() {
+    this._adminServices.getAllcourses().subscribe((data) => {
+      this.numOfCourse = data.numOfCourses;
+      this.charts();
+    });
+
+  }
+  getAllStudents() {
+    this._adminServices.getAllStudents().subscribe((data) => {
+      this.numOfSutudents = data.numOfStudent;
+      this.charts();
+    });
+
+  }
+  // 3 function get all information about the instructors,courses and students took only the count of each to fill the data of the chart 
+
+  charts() {
+    this.data = {
+      labels: ['Students', 'Courses', 'Instructors'],
+      datasets: [
+        {
+          data: [this.numOfSutudents, this.numOfCourse, this.numOfInstructors],
+          backgroundColor: [
+            "#FF6384",
+            "#36A2EB",
+            "#FFCE56"
+          ],
+          hoverBackgroundColor: [
+            "#FF6384",
+            "#36A2EB",
+            "#FFCE56"
+          ]
+        }]
+    };
   }
 
   ngOnInit(): void {
